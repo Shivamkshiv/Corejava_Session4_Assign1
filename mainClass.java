76545c6cffc81464a121302210784bc52939b6b0
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-09 17:15:10 +0530

    Create mainClass.java

---
+++ mainClass.java
@@ -0,0 +1,20 @@
+package samePackage;
+
+
+public class mainClass {
+
+	int a= 10;
+	int b =20;
+	
+	
+	public int calculateSum(){
+		
+        int sum = a + b ;
+		System.out.println(sum);
+		
+		return sum;
+		
+		
+	}
+	
+}
