6dee70719ade63db9bfc3c3d1e9021ae54968dc8
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-09 17:14:22 +0530

    Create diffpackagesubClass.java

---
+++ diffpackagesubClass.java
@@ -0,0 +1,17 @@
+package diffPackage;
+
+import samePackage.mainClass;
+
+public class diffpackagesubClass extends mainClass  {
+	
+	
+     public static void main(String[] args) {
+		
+    	 diffpackagesubClass ab = new diffpackagesubClass();
+    	 ab.calculateSum();
+    	 
+    	 
+	}	
+	
+
+}
